e10022e35ef0304ee348b7c99a0ac308a4ffab20
Sergey Brovko <serbro7@gmail.com>
2025-10-23 18:08:16 +0200

    Counter component created

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './components/counter';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, CounterComponent],
+  templateUrl: './app.html'
 })
 export class App {
   protected readonly title = signal('4-counter-app');
---
+++ src/app/components/counter.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  imports: [],
+  templateUrl: './counter.html'
+})
+export class CounterComponent {
+
+  title: String = 'Counter using Redux';
+
+  counter: number;
+
+  //Instead of initialisin the counter variable in the variables declaration, we initialise it in the constructor.
+  constructor() {
+    this.counter = 0;
+  }
+
+  increase(): void {
+    this.counter++;
+    console.log('increasing ...');
+  }
+
+  decrease(): void {
+    this.counter--;
+    console.log('decreasing ...');
+  }
+
+  reset(): void {
+    this.counter = 0;
+    console.log('resetting counter ...');
+  }
+}
