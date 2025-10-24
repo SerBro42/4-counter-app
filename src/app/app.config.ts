8b65c10dbf68abf7653168a290ab4bccda00e9ab
Sergey Brovko <serbro7@gmail.com>
2025-10-24 11:37:59 +0200

    Redux DevTools implemented

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { counterReducer } from './store/items.reducer';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,7 +12,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideStore({
-      counter: counterReducer
-    })
+        counter: counterReducer
+    }),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 };
